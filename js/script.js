3a35d10b919de50b2db255503bb31f530107e243
amogus <vmundirekartofel@gmail.com>
2021-06-06 11:04:43 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function ())
+  new SimpleAdaptiveSlider('#slider-1', {
+    loop: true,
+    autoplay: false,
+    interval: 5000,
+    swipe: true,
+  });
